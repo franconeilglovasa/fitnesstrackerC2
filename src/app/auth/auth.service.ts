738754ad0ee8c44c6ba01fb66d5f48168bfd46ad
Franco Neil Glovasa <franconeilglovasa@outlook.com>
2021-08-27 09:41:37 +0800

    End Of Chapter 6

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,47 +13,66 @@ export class AuthService {
   //private user: User;
   private isAuthenticated = false;
 
-  constructor(private router: Router, 
+  constructor(private router: Router,
     private afAuth: AngularFireAuth,
-    private trainingService: TrainingService) {}
+    private trainingService: TrainingService) { }
+
+
+
+  initAuthListener() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
+      } else {
+        this.trainingService.cancelSubscriptions();
+        this.authChange.next(false);
+        this.router.navigate(['/login']);
+        this.isAuthenticated = false;
+      }
+    });
+  }
 
   registerUser(authData: AuthData) {
 
     this.afAuth.createUserWithEmailAndPassword(authData.email, authData.password)
-    .then(result => {
-      console.log (result);
-      this.authSuccessfully();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(result => {
+        console.log(result);
+        this.authSuccessfully();
+      })
+      .catch(error => {
+        console.log(error);
+      });
     // this.user = {
     //   email: authData.email,
     //   userId: Math.round(Math.random() * 10000).toString()
     // };
-    this.authSuccessfully();
+    
+    //this.authSuccessfully();
   }
 
   login(authData: AuthData) {
     this.afAuth
-    .signInWithEmailAndPassword(authData.email, authData.password)
-    .then(result => {
-      console.log(result);
-      this.authSuccessfully();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .signInWithEmailAndPassword(authData.email, authData.password)
+      .then(result => {
+        console.log(result);
+        //this.authSuccessfully();
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   logout() {
-    this.trainingService.cancelSubscriptions();
-    this.authChange.next(false);
-    this.router.navigate(['/login']);
-    this.isAuthenticated = false;
+    // this.trainingService.cancelSubscriptions();
+    // this.authChange.next(false);
+    // this.router.navigate(['/login']);
+    // this.isAuthenticated = false;
+    this.afAuth.signOut();
   }
 
-  
+
 
   isAuth() {
     return this.isAuthenticated;
